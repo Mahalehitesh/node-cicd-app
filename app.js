1fdbdc70db207be206166ce71fb3210f73118fb8
Hitesh Mahale <mahalehitesh746@gmail.com>
2026-04-24 14:46:18 +0530

    fixed server binding for EC2

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
+// Route
 app.get("/", (req, res) => {
   res.send("CI/CD Pipeline Working 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// IMPORTANT: Bind to 0.0.0.0 for EC2
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
